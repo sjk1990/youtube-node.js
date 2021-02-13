618936193dfa66ad1962d7f8f4e1dd0472307604
SJKim <worldsjk1990@gmail.com>
2021-02-14 00:09:16 +0900

    in MVC model, make Router & Controller

--- app.js
+++ app.js
@@ -6,36 +6,22 @@ import helmet from "helmet";
 //parser
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router";
+//route
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
 
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from home!");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
-//middleware 마지막 함수 실행 전 원하는 작업 수행등이 목적(user login 여부 체크 등...)
-const middleware = (req, res, next) => {
-  console.log("this is middleware");
-  next();
-};
-
-//middleware
-//순서대로 진행
-//모든 route에 대해 실행, 원하는 위치에 두고 목적에 맞게 실행 가능
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
-app.use(middleware);
 
-//해당 route에서만 실행
-app.get("/", middleware, handleHome);
-
-app.get("/profile", handleProfile);
-
-//app.get , app.use의 차이점 => use: /user 경로로 들어왔을 때 userRouter의 모든 함수(router)를 사용하겠다는 의미
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
--- router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-//export default 일 경우 ==> import app from './app'과 같은 형식으로 import
-//아래와 같을 경우 ==> import {userRouter} from './router'과 같이 import
-export const userRouter = express.Router();
-
-userRouter.get("/", (req, res) => res.send("user index"));
-userRouter.get("/edit", (req, res) => res.send("user edit"));
-userRouter.get("/password", (req, res) => res.send("user password"));
