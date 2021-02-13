277fda5580e3834d6140ddb562d14e1351aca18f
SJKim <worldsjk1990@gmail.com>
2021-02-13 22:53:06 +0900

    seperate app & init

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 
 const app = express();
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from home!");
 
@@ -38,4 +34,4 @@ app.get("/", middleware, handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+export default app;
