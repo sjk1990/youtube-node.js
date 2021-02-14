d32e468925c7393570d2dba453ea6bc1f5679ac7
SJKim <worldsjk1990@gmail.com>
2021-02-14 18:36:30 +0900

    in MVC, setting view part with pug-view engine

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ import routes from "./routes";
 
 const app = express();
 
+//set view engine - pug
+app.set("view engine", "pug");
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
