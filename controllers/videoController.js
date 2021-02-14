34cbc16ad36750c7171ccc760e1152d60dacf71c
SJKim <worldsjk1990@gmail.com>
2021-02-15 02:02:43 +0900

    how to use: template variables in pug

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,4 @@
-export const home = (req, res) => res.render("home");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.render("search");
 export const upload = (req, res) => res.render("upload");
 export const videoDetail = (req, res) => res.render("videoDetail");
