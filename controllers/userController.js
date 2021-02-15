2215c7c1af8a6a56698743d89a5f51b7b1b02c9d
SJKim <worldsjk1990@gmail.com>
2021-02-15 12:31:47 +0900

    handle join controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,30 @@
-export const join = (req, res) => res.render("join");
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  //   console.log(req.body);
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // To Do: Register User
+    // To Do: Log user in
+    res.redirect(routes.home);
+  }
+};
+
 export const login = (req, res) => res.render("login");
+
 export const logout = (req, res) => res.render("logout");
+
 export const userDetail = (req, res) => res.render("userDetail");
+
 export const editProfile = (req, res) => res.render("editProfile");
+
 export const changePassword = (req, res) => res.render("changePassword");
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,14 +3,19 @@ import { videos } from "../db";
 export const home = (req, res) => {
   res.render("home", { pageTitle: "Home", videos });
 };
+
 export const search = (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
   //const searchingBy = req.query.term 과 동일한 ES6 문법
-  res.render("search", { pageTitle: "Search", searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
+
 export const upload = (req, res) => res.render("upload");
+
 export const videoDetail = (req, res) => res.render("videoDetail");
+
 export const editVideo = (req, res) => res.render("editVideo");
+
 export const deleteVideo = (req, res) => res.render("deleteVideo");
