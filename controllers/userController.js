23df88ded03ce6bf446f62772a395249f4082c6c
SJKim <worldsjk1990@gmail.com>
2021-02-15 17:57:00 +0900

    handle controllers & required input option

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,7 +26,10 @@ export const postLogin = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const logout = (req, res) => res.render("logout");
+export const logout = (req, res) => {
+  // To Do: Process Log Out
+  res.redirect(routes.home);
+};
 
 export const userDetail = (req, res) => res.render("userDetail");
 
