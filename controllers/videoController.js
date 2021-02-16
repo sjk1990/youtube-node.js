bba2090102efbdaf9d0c3bc385c454d379b284f6
SJKim <worldsjk1990@gmail.com>
2021-02-16 13:05:03 +0900

    delete video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -44,7 +44,7 @@ export const videoDetail = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
-    res.render("videoDetail", { pageTitle: "Video Detail", video });
+    res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     res.redirect(routes.home);
   }
@@ -76,5 +76,12 @@ export const postEditVideo = async (req, res) => {
     res.redirect(routes.home);
   }
 };
-
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    await Video.findOneAndRemove({ _id: id });
+  } catch (error) {}
+  res.redirect(routes.home);
+};
