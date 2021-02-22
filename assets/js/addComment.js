6034b51ec50edf8d06ad9dcbba28eeab3a222c2a
SJKim <worldsjk1990@gmail.com>
2021-02-22 17:45:13 +0900

    add comment & render like realtime

---
+++ assets/js/addComment.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+const addCommentForm = document.getElementById("jsAddComment");
+const commentList = document.getElementById("jsCommentList");
+const commentNumber = document.getElementById("jsCommentNumber");
+
+const increaseNumber = () => {
+  commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
+};
+
+// db에 성공적으로 comment가 보내지면 새로고침없이 바로 해당 코멘트를 추가하여
+// 실시간 댓글 업데이트 처럼 보이게 함.
+const addComment = (comment) => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerHTML = comment;
+  li.appendChild(span);
+  commentList.prepend(li);
+  increaseNumber();
+};
+
+const sendComment = async (comment) => {
+  const videoId = window.location.href.split("/videos/")[1];
+  const response = await axios({
+    url: `/api/${videoId}/comment`,
+    method: "POST",
+    data: {
+      comment,
+    },
+  });
+  if (response.status === 200) {
+    addComment(comment);
+  }
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const commentInput = addCommentForm.querySelector("input");
+  const comment = commentInput.value;
+  sendComment(comment);
+  commentInput.value = "";
+};
+
+function init() {
+  addCommentForm.addEventListener("submit", handleSubmit);
+}
+
+if (addCommentForm) {
+  init();
+}
--- routes.js
+++ routes.js
@@ -28,6 +28,7 @@ const GITHUB_CALLBACK = "/auth/github/callback";
 // do not render page
 const API = "/api";
 const REGISTER_VIEW = "/:id/view";
+const ADD_COMMENT = "/:id/comment";
 
 const routes = {
   home: HOME,
@@ -73,6 +74,7 @@ const routes = {
   me: ME,
   api: API,
   registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT,
 };
 
 export default routes;
